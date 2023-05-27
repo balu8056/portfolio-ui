d24b9711901a799b2aeb7af6ded9633d1cfc4b15
Balasubramani R <46841670+balu8056@users.noreply.github.com>
2023-05-27 20:41:23 +0530

    updated

--- pages/about.tsx
+++ pages/about.tsx
@@ -41,7 +41,7 @@ const About = () => {
           <Grid container spacing={2}>
             <Grid item sm spacing={2} container direction="column" justifyContent="center" alignItems="center">
               <Grid item marginTop={2}>
-                {isAboutImgLoaded ? (
+                {!isAboutImgLoaded ? (
                   <Skeletons type="image" />
                 ) : (
                   <Image
